ab907f76037b7b397e9520fb4ef75941cb4de535
romain <julian-romain@hotmail.fr>
2017-12-11 11:05:05 +0100

    Initial commit

---
+++ app/src/main/java/touchpgm/ihm/the/touchpgm/MainActivity.java
@@ -0,0 +1,13 @@
+package touchpgm.ihm.the.touchpgm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
