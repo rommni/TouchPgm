faa674e7df4bb3983bc50a6a24694691c31b1fe6
romain <julian-romain@hotmail.fr>
2018-01-13 13:23:58 +0100

    mise en place min et max pour secondes

--- app/src/main/java/touchpgm/ihm/the/touchpgm/Accueil.java
+++ app/src/main/java/touchpgm/ihm/the/touchpgm/Accueil.java
@@ -1,25 +1,41 @@
 package touchpgm.ihm.the.touchpgm;
 
 import android.content.Intent;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputFilter;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import touchpgm.ihm.the.touchpgm.touchpgm.ihm.the.touchpgm.filter.InputFilterMinMax;
 
 public class Accueil extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final Resources res = getResources();
+        final int minSeconde = res.getInteger(R.integer.min_secondes);
+        final int maxSeconde = res.getInteger(R.integer.max_secondes);
+
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
+
         final TextView time = findViewById(R.id.time);
         time.setText("5");
-        Button button = findViewById(R.id.button);
+        time.setFilters(new InputFilter[]{new InputFilterMinMax(minSeconde,maxSeconde)});
+
+        final TextView consigneSeconde = findViewById(R.id.consigne_seconde);
+        String consigneSecondeString = getString(R.string.Consigne_seconde, minSeconde, maxSeconde);
+        consigneSeconde.setText(consigneSecondeString);
+
+        final Button button = findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
-
                 Intent intent = new Intent(Accueil.this, InGame.class);
                 Bundle b = new Bundle();
                 b.putString("time", time.getText().toString()); //Your id
---
+++ app/src/main/java/touchpgm/ihm/the/touchpgm/touchpgm/ihm/the/touchpgm/filter/InputFilterMinMax.java
@@ -0,0 +1,30 @@
+package touchpgm.ihm.the.touchpgm.touchpgm.ihm.the.touchpgm.filter;
+
+import android.text.InputFilter;
+import android.text.Spanned;
+
+
+public class InputFilterMinMax implements InputFilter {
+    private int min;
+    private int max;
+
+    public InputFilterMinMax(int min, int max) {
+        this.min = min;
+        this.max = max;
+    }
+
+    @Override
+    public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+        //noinspection EmptyCatchBlock
+        try {
+            int input = Integer.parseInt(dest.subSequence(0, dstart).toString() + source + dest.subSequence(dend, dest.length()));
+            if (isInRange(min, max, input))
+                return null;
+        } catch (NumberFormatException nfe) { }
+        return "";
+    }
+
+    private boolean isInRange(int a, int b, int c) {
+        return b > a ? c >= a && c <= b : c >= b && c <= a;
+    }
+}
